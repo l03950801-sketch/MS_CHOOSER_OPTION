1e7c6679ccb520082c522a3996d9ecb949cbd210
Yoyo <yoyo@Yoyodebndiannao.tpg_nbn>
2026-03-30 21:37:15 +1100

    sentiment data added

--- src/download_data.py
+++ src/download_data.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 from fredapi import Fred
 from pathlib import Path
+import requests
 
 # Configuration
 
@@ -9,8 +10,10 @@ START_DATE = "2018-01-01"
 END_DATE = "2024-12-31"
 
 FRED_API_KEY = "926c9e047076f239237763f6a03961da"
+AV_API_KEY = "RXP2APE979NRUVVT"
 
 OUTPUT_PATH = Path("data/raw_data.csv")
+
 
 # Download financial data
 
@@ -37,13 +40,17 @@ def download_vix_data():
 
     return vix
 
+
 # Download macroeconomic data
 
 def download_treasury_data():
+
     print("Downloading Treasury yield data...")
+
     fred = Fred(api_key=FRED_API_KEY)
 
     treasury = fred.get_series("DGS10")
+
     treasury = treasury.to_frame(name="treasury_rate")
 
     treasury.index = pd.to_datetime(treasury.index)
@@ -51,9 +58,47 @@ def download_treasury_data():
     return treasury
 
 
+# Download sentiment data
+
+def download_sentiment_data():
+
+    print("Downloading news sentiment data...")
+
+    url = (
+        "https://www.alphavantage.co/query?"
+        f"function=NEWS_SENTIMENT&tickers=JPM"
+        f"&time_from=20180101T0000"
+        f"&time_to=20241231T0000"
+        f"&apikey={AV_API_KEY}"
+    )
+
+    response = requests.get(url).json()
+
+    articles = response.get("feed", [])
+
+    sentiment_data = []
+
+    for article in articles:
+
+        date = article["time_published"][:8]
+
+        score = article["overall_sentiment_score"]
+
+        sentiment_data.append({
+            "date": pd.to_datetime(date),
+            "sentiment": score
+        })
+
+    sentiment_df = pd.DataFrame(sentiment_data)
+
+    sentiment_df = sentiment_df.groupby("date").mean()
+
+    return sentiment_df
+
+
 # Merge datasets
 
-def merge_data(jpm, vix, treasury):
+def merge_data(jpm, vix, treasury, sentiment):
 
     print("Merging datasets...")
 
@@ -66,7 +111,15 @@ def merge_data(jpm, vix, treasury):
         how="left"
     )
 
+    data = data.merge(
+        sentiment,
+        left_index=True,
+        right_index=True,
+        how="left"
+    )
+
     data.reset_index(inplace=True)
+
     data.rename(columns={"Date": "date"}, inplace=True)
 
     return data
@@ -79,16 +132,21 @@ def main():
     print("Starting data pipeline...")
 
     jpm = download_stock_data()
+
     vix = download_vix_data()
+
     treasury = download_treasury_data()
 
-    dataset = merge_data(jpm, vix, treasury)
+    sentiment = download_sentiment_data()
+
+    dataset = merge_data(jpm, vix, treasury, sentiment)
 
     OUTPUT_PATH.parent.mkdir(parents=True, exist_ok=True)
 
     dataset.to_csv(OUTPUT_PATH, index=False)
 
     print("Data saved to:", OUTPUT_PATH)
+
     print("Pipeline completed successfully.")
 
 
